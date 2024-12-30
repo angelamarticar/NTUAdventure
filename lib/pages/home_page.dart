cf5965489c459b3cf62e470d6dd4fc2e3886db21
angmarcar6 <angmarcar6@alum.us.es>
2024-12-30 13:34:23 +0200

    commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../theme/app_decoration.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Page!',
+        style: TextStyle(fontSize: 50.0),),
+      ),
+    );
+  }
+}
