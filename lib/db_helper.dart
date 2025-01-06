2d0ee45efee0eae5298e5eb5ddeacb7835583884
Julia Vogel <julia.vogel@stud.tu-darmstadt.de>
2025-01-06 18:25:40 +0200

    adaptations dDB - included pictures and foreign keys, dummy data population

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -16,260 +16,176 @@ class DatabaseHelper {
   }
 
   Future<Database> _initDatabase() async {
-    try {
-      String dbPath = await getDatabasesPath();
-      String path = join(dbPath, 'app_database.db');
-      return await openDatabase(
-        path,
-        version: 1,
-        onCreate: _onCreate,
-        onConfigure: _onConfigure,
-      );
-    } catch (e) {
-      throw Exception("Error initializing database: $e");
+    String dbPath = await getDatabasesPath();
+    String path = join(dbPath, 'app_database.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+      onUpgrade: _onUpgrade,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await _executeCreateTables(db);
+    await _insertDummyData(db);
+  }
+
+  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    // Drop all tables and recreate
+    await _dropAllTables(db);
+    await _onCreate(db, newVersion);
+  }
+
+  Future<void> _dropAllTables(Database db) async {
+    final tables = ['users', 'courses', 'ratings', 'events', 'trip_signups'];
+    for (String table in tables) {
+      await db.execute('DROP TABLE IF EXISTS $table');
     }
   }
 
-  Future<void> _onConfigure(Database db) async {
-    await db.execute('PRAGMA foreign_keys = ON');
+  Future<void> resetDatabase() async {
+  final db = await database;
+  await _dropAllTables(db);  // Drop all tables
+  await _executeCreateTables(db);  // Recreate tables
+  await _insertDummyData(db);  // Repopulate with dummy data
+}
+
+
+  Future<void> _executeCreateTables(Database db) async {
+    await db.execute('''
+      CREATE TABLE users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        user_type TEXT CHECK(user_type IN ('erasmusStudent', 'professor', 'esnOrganiser')) NOT NULL,
+        profile_picture_path TEXT
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE courses(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        description TEXT,
+        school TEXT,
+        language TEXT,
+        semester TEXT,
+        image_path TEXT,
+        professor_id INTEGER,
+        FOREIGN KEY (professor_id) REFERENCES users (id)
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE ratings(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        workload INTEGER,
+        difficulty INTEGER,
+        overall_rating INTEGER,
+        comment TEXT,
+        course_id INTEGER,
+        FOREIGN KEY (course_id) REFERENCES courses (id)
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE events(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        date TEXT NOT NULL,
+        description TEXT,
+        price DOUBLE,
+        location TEXT,
+        image_path TEXT
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE trip_signups(
+        trip_id INTEGER,
+        user_id INTEGER,
+        signup_date TEXT NOT NULL,
+        PRIMARY KEY (trip_id, user_id),
+        FOREIGN KEY (trip_id) REFERENCES events (id) ON DELETE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+      )
+    ''');
   }
 
-  FutureOr<void> _onCreate(Database db, int version) async {
-    try {
-      // Create Users Table
-      await db.execute('''
-        CREATE TABLE users(
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT NOT NULL,
-          email TEXT NOT NULL UNIQUE,
-          password TEXT NOT NULL,
-          user_type TEXT CHECK(user_type IN ('student', 'professor')) NOT NULL
-        )
-      ''');
-
-      // Create Courses Table
-      await db.execute('''
-        CREATE TABLE courses(
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT NOT NULL,
-          description TEXT,
-          school TEXT
-        )
-      ''');
-
-      // Create Ratings Table
-      await db.execute('''
-        CREATE TABLE ratings(
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          workload INTEGER,
-          difficulty INTEGER,
-          overall_rating INTEGER,
-          language TEXT,
-          semester TEXT,
-          comment TEXT,
-          course_id INTEGER,
-          FOREIGN KEY (course_id) REFERENCES courses (id) ON DELETE CASCADE
-        )
-      ''');
-
-      // Create Events Table
-      await db.execute('''
-        CREATE TABLE events(
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          title TEXT NOT NULL,
-          date TEXT NOT NULL,
-          description TEXT,
-          price DOUBLE,
-          location TEXT
-        )
-      ''');
-
-      // Create Trip Signups Table
-      await db.execute('''
-        CREATE TABLE trip_signups(
-          signup_date TEXT NOT NULL,
-          PRIMARY KEY (trip_id, user_id),
-          FOREIGN KEY (trip_id) REFERENCES events (id) ON DELETE CASCADE,
-          FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-        )
-      ''');
-    } catch (e) {
-      throw Exception("Error creating tables: $e");
-    }
-  }
-  /*
-  Future<void> insertDummyData() async {
+  Future<int> insert(String table, Map<String, dynamic> values) async {
     final db = await database;
-    Batch batch = db.batch();
-
-    // Add dummy users
-    List<Map<String, dynamic>> users = [
-      {
-        'name': 'Ada Lovelace',
-        'email': 'ada@computerscience.com',
-        'password': 'analyticalengine',
-        'user_type': 'professor',
-      },
-      {
-        'name': 'Alan Turing',
-        'email': 'turing@computerscience.com',
-        'password': 'enigma123',
-        'user_type': 'professor',
-      },
-      {
-        'name': 'Grace Hopper',
-        'email': 'hopper@computerscience.com',
-        'password': 'cobolrocks',
-        'user_type': 'student',
-      },
-      {
-        'name': 'Katherine Johnson',
-        'email': 'johnson@nasa.com',
-        'password': 'mathematician',
-        'user_type': 'student',
-      },
-    ];
-
-    for (var user in users) {
-      batch.insert('users', user);
-    }
-
-    // Add dummy courses
-    List<Map<String, dynamic>> courses = [
-      {
-        'name': 'Introduction to Quantum Computing',
-        'description': 'Learn the basics of quantum mechanics and computing.',
-        'school': 'Physics and Computer Science',
-      },
-      {
-        'name': 'Artificial Intelligence Ethics',
-        'description': 'Explore the ethical implications of AI development.',
-        'school': 'Philosophy',
-      },
-      {
-        'name': 'History of Space Exploration',
-        'description': 'An overview of humanity’s journey to the stars.',
-        'school': 'Astronomy',
-      },
-      {
-        'name': 'Advanced Data Structures',
-        'description': 'Study and implement complex data structures.',
-        'school': 'Computer Science',
-      },
-      {
-        'name': 'Astrobiology',
-        'description': 'Explore the possibilities of life beyond Earth.',
-        'school': 'Biology and Astronomy',
-      },
-    ];
-
-    for (var course in courses) {
-      batch.insert('courses', course);
-    }
-
-    // Add dummy ratings
-    List<Map<String, dynamic>> ratings = [
-      {
-        'workload': 6,
-        'difficulty': 7,
-        'overall_rating': 8,
-        'language': 'English',
-        'semester': 'Winter',
-        'comment': 'Challenging but worth it!',
-        'course_id': 1,
-      },
-      {
-        'workload': 5,
-        'difficulty': 5,
-        'overall_rating': 9,
-        'language': 'English',
-        'semester': 'Summer',
-        'comment': 'Engaging and thought-provoking.',
-        'course_id': 2,
-      },
-      {
-        'workload': 7,
-        'difficulty': 8,
-        'overall_rating': 7,
-        'language': 'English',
-        'semester': 'Winter',
-        'comment': 'Fascinating content with tough assignments.',
-        'course_id': 3,
-      },
-      {
-        'workload': 4,
-        'difficulty': 6,
-        'overall_rating': 8,
-        'language': 'Greek',
-        'semester': 'Summer',
-        'comment': 'Clear explanations and hands-on examples.',
-        'course_id': 4,
-      },
-      {
-        'workload': 8,
-        'difficulty': 9,
-        'overall_rating': 9,
-        'language': 'Greek',
-        'semester': 'Winter',
-        'comment': 'A very advanced and rewarding course!',
-        'course_id': 5,
-      },
-    ];
-
-    for (var rating in ratings) {
-      batch.insert('ratings', rating);
-    }
-
-    // Add dummy events
-    List<Map<String, dynamic>> events = [
-      {
-        'title': 'Federation Science Symposium',
-        'date': '2025-02-01',
-        'description': 'Explore the latest scientific discoveries in the galaxy.',
-        'price': 50.0,
-        'location': 'Starbase 1',
-      },
-      {
-        'title': 'Klingon Cultural Festival',
-        'date': '2025-03-15',
-        'description': 'Celebrate the rich traditions of the Klingon Empire.',
-        'price': 20.0,
-        'location': 'QonoS',
-      },
-      {
-        'title': 'Starship Piloting Workshop',
-        'date': '2025-04-10',
-        'description': 'Hone your starship piloting skills with experts.',
-        'price': 100.0,
-        'location': 'Starfleet Academy',
-      },
-    ];
-
-    for (var event in events) {
-      batch.insert('events', event);
-    }
-
-    // Execute the batch
-    await batch.commit(noResult: true);
-}*/
-
+    return await db.insert(table, values);
+  }
 
   Future<List<Map<String, dynamic>>> getAll(String table) async {
-    try {
-      final db = await database;
-      return await db.query(table);
-    } catch (e) {
-      throw Exception("Error fetching all from $table: $e");
-    }
+    final db = await database;
+    return await db.query(table);
   }
 
-  Future<void> deleteDatabaseFile() async {
-    try {
-      String dbPath = await getDatabasesPath();
-      String path = join(dbPath, 'app_database.db');
-      await deleteDatabase(path);
-    } catch (e) {
-      throw Exception("Error deleting database file: $e");
-    }
+  Future<Map<String, dynamic>?> getById(String table, int id) async {
+    final db = await database;
+    final result = await db.query(table, where: 'id = ?', whereArgs: [id]);
+    return result.isNotEmpty ? result.first : null;
+  }
+
+  Future<int> update(String table, Map<String, dynamic> values) async {
+    final db = await database;
+    return await db.update(table, values, where: 'id = ?', whereArgs: [values['id']]);
+  }
+
+  Future<int> delete(String table, int id) async {
+    final db = await database;
+    return await db.delete(table, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> deleteComposite(String table, Map<String, dynamic> keys) async {
+    final db = await database;
+    final whereClause = keys.keys.map((key) => '$key = ?').join(' AND ');
+    final whereArgs = keys.values.toList();
+    return await db.delete(table, where: whereClause, whereArgs: whereArgs);
+  }
+
+  Future<void> _insertDummyData(Database db) async {
+    // Add Star Trek-themed users
+    await db.insert('users', {'name': 'Jean-Luc Picard', 'email': 'picard@starfleet.com', 'password': 'enterpriseD', 'user_type': 'professor'});
+    await db.insert('users', {'name': 'Spock', 'email': 'spock@vulcan.com', 'password': 'logicRules', 'user_type': 'professor'});
+    await db.insert('users', {'name': 'James T. Kirk', 'email': 'kirk@starfleet.com', 'password': 'tosLegend', 'user_type': 'professor'});
+    await db.insert('users', {'name': 'Data', 'email': 'data@starfleet.com', 'password': 'positronic', 'user_type': 'erasmusStudent'});
+    await db.insert('users', {'name': 'Seven of Nine', 'email': 'seven@borg.com', 'password': 'annika', 'user_type': 'erasmusStudent'});
+    await db.insert('users', {'name': 'Kathryn Janeway', 'email': 'janeway@voyager.com', 'password': 'deltaQuadrant', 'user_type': 'professor'});
+
+    // Add Star Trek-themed courses
+    await db.insert('courses', {'name': 'Warp Core Engineering', 'description': 'Learn to manage starship warp cores.', 'school': 'Engineering', 'language': 'English', 'semester': 'Winter'});
+    await db.insert('courses', {'name': 'Temporal Mechanics', 'description': 'The study of time travel and temporal anomalies.', 'school': 'Physics', 'language': 'Vulcan', 'semester': 'Spring'});
+    await db.insert('courses', {'name': 'Federation Diplomacy', 'description': 'Understand interplanetary relations and diplomacy.', 'school': 'Political Science', 'language': 'English', 'semester': 'Summer'});
+    await db.insert('courses', {'name': 'Borg Technology', 'description': 'Analysis of Borg nanoprobes and adaptation systems.', 'school': 'Cybernetics', 'language': 'English', 'semester': 'Fall'});
+    await db.insert('courses', {'name': 'Starship Navigation', 'description': 'Advanced techniques in starship piloting.', 'school': 'Astronomy', 'language': 'English', 'semester': 'Winter'});
+    await db.insert('courses', {'name': 'Holodeck Programming', 'description': 'Learn to create and maintain holodeck programs.', 'school': 'Computer Science', 'language': 'English', 'semester': 'Spring'});
+
+    // Add Star Trek-themed ratings
+    await db.insert('ratings', {'workload': 8, 'difficulty': 7, 'overall_rating': 9, 'comment': 'Warp cores are fascinating!', 'course_id': 1});
+    await db.insert('ratings', {'workload': 6, 'difficulty': 9, 'overall_rating': 8, 'comment': 'Temporal mechanics are complex but intriguing.', 'course_id': 2});
+    await db.insert('ratings', {'workload': 7, 'difficulty': 5, 'overall_rating': 10, 'comment': 'Diplomacy with Klingons was fun!', 'course_id': 3});
+    await db.insert('ratings', {'workload': 9, 'difficulty': 10, 'overall_rating': 7, 'comment': 'Borg tech is incredibly advanced!', 'course_id': 4});
+    await db.insert('ratings', {'workload': 5, 'difficulty': 5, 'overall_rating': 8, 'comment': 'Starship piloting is exhilarating.', 'course_id': 5});
+    await db.insert('ratings', {'workload': 4, 'difficulty': 6, 'overall_rating': 9, 'comment': 'Holodeck programming was creative!', 'course_id': 6});
+
+    // Add Star Trek-themed events
+    await db.insert('events', {'title': 'Klingon Bat’leth Tournament', 'date': '2025-06-01', 'description': 'Compete in a Bat’leth combat tournament.', 'price': 30.0, 'location': 'Qo’noS'});
+    await db.insert('events', {'title': 'Federation Science Symposium', 'date': '2025-07-15', 'description': 'Explore the latest Federation science.', 'price': 50.0, 'location': 'Starbase 1'});
+    await db.insert('events', {'title': 'Delta Quadrant Exploration', 'date': '2025-08-10', 'description': 'A guided tour of the Delta Quadrant.', 'price': 100.0, 'location': 'Voyager'});
+    await db.insert('events', {'title': 'Holodeck Creative Workshop', 'date': '2025-09-05', 'description': 'Create your own holodeck adventures.', 'price': 20.0, 'location': 'Enterprise-D'});
+    await db.insert('events', {'title': 'Vulcan Logic Retreat', 'date': '2025-10-12', 'description': 'Immerse yourself in Vulcan philosophy.', 'price': 15.0, 'location': 'Vulcan'});
+    await db.insert('events', {'title': 'Starfleet Graduation Ceremony', 'date': '2025-11-20', 'description': 'Celebrate the newest Starfleet cadets.', 'price': 0.0, 'location': 'Starfleet Academy'});
+
+    // Add Star Trek-themed trip signups
+    await db.insert('trip_signups', {'trip_id': 1, 'user_id': 1, 'signup_date': '2025-05-20'});
+    await db.insert('trip_signups', {'trip_id': 2, 'user_id': 3, 'signup_date': '2025-07-01'});
+    await db.insert('trip_signups', {'trip_id': 3, 'user_id': 5, 'signup_date': '2025-08-05'});
+    await db.insert('trip_signups', {'trip_id': 4, 'user_id': 6, 'signup_date': '2025-09-01'});
+    await db.insert('trip_signups', {'trip_id': 5, 'user_id': 2, 'signup_date': '2025-10-01'});
+    await db.insert('trip_signups', {'trip_id': 6, 'user_id': 4, 'signup_date': '2025-11-10'});
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,72 @@
+
 import 'package:flutter/material.dart';
-import 'package:ntuadventure/pages/course_page.dart'; // Import the CoursePage
+import 'package:ntuadventure/pages/calendar_page.dart';
+import 'package:ntuadventure/pages/home_page.dart';
+import 'package:ntuadventure/pages/login_page.dart';
+import 'package:ntuadventure/pages/map_page.dart';
+import 'package:ntuadventure/pages/course_page.dart';
+import 'package:ntuadventure/theme/theme_helper.dart';
 import 'package:ntuadventure/db_helper.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initializeDatabase(); // Initialize the database
-  runApp(CourseApp());
-}
 
-Future<void> initializeDatabase() async {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter bindings are initialized
   final dbHelper = DatabaseHelper();
-  await dbHelper.database; // Ensure the database is created
-  //await dbHelper.insertDummyData(); // Populate the database with dummy data
+
+  await dbHelper.resetDatabase(); // Reset database
+  await printDatabaseContents();
+  runApp(CourseApp());
 }
 
 class CourseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Course Explorer',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: CoursePage(), // Set the home page to CoursePage
+      title: 'NTUAdventure',
+      theme: ThemeHelper().themedata(),
+      home: HomePage(), // Set the default home page
+      debugShowCheckedModeBanner: false, // Disable the debug banner
+    );
+  }
+}
+
+Future<void> printDatabaseContents() async {
+  final dbHelper = DatabaseHelper();
+  final db = await dbHelper.database;
+
+  // List of tables to query
+  final tables = ['users', 'courses', 'ratings', 'events', 'trip_signups'];
+
+  print('\n======= DATABASE CONTENT =======');
+  for (String table in tables) {
+    print('\nTable: $table');
+    final rows = await dbHelper.getAll(table);
+    if (rows.isEmpty) {
+      print('No data found.');
+    } else {
+      for (var row in rows) {
+        print(row);
+      }
+    }
+  }
+  print('======= END OF DATABASE =======\n');
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Database Printer',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Database Printer'),
+        ),
+        body: Center(
+          child: Text('Check your terminal for the database content!'),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
